37b8d4c2a592a77da311a3da4b84e6338909c575
Sean Miller <trickygopher2@protonmail.com>
2020-06-02 15:52:27 -0700

    add jest

---
+++ src/lib/test/constants.test.js
@@ -0,0 +1,29 @@
+// import { THEMES } from '../constants/index';
+
+// describe('Theme constants', () => {
+//   it('should have 2 properties', () => {
+//     expect(Object.keys(THEMES).length).toBe(2);
+//   });
+
+//   it('should have properties free and premium', () => {
+//     expect(Object.keys(THEMES)).toBe(['free', 'premium']);
+//   });
+
+//   it('should have 2 free themes', () => {
+//     expect(Object.keys(THEMES.free).length).toBe(2);
+//   });
+
+//   it('should have correct free themes', () => {
+//     expect(Object.keys(THEMES.free)).toBe(['LIGHT', 'DARK']);
+//   });
+
+//   it('should have 6 premium themes', () => {
+//     expect(Object.keys(THEMES.premium).length).toBe(6);
+//   });
+
+//   it('should have correct free themes', () => {
+//     const { premium } = THEMES;
+//     expect(Object.keys(premium)).toBe(['NIGHT', 'POLAR', 'NINJA', 'FIZZ', 'BUMBLEBEE', 'GUNMETAL']);
+//   });
+
+// });
---
+++ src/lib/test/rangeMappings.test.js
@@ -0,0 +1,12 @@
+// import rangeMappings from '../rangeMappings';
+
+// describe('Range mappings', () => {
+
+//   it('should be an array', () => {
+//     expect(Array.isArray(rangeMappings)).toBe(true);
+//   });
+
+//   it('should have 7 range options', () => {
+//     expect(rangeMappings.length).toBe(7);
+//   });
+// });
