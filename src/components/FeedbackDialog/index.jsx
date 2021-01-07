8ec3283fbc0c32c8efacc0804f95f9a5fbdc9827
Sean Miller <trickygopher2@protonmail.com>
2021-01-06 23:26:29 -0500

    feedback form handling

--- src/components/FeedbackDialog/index.jsx
+++ src/components/FeedbackDialog/index.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
+import { makeStyles } from '@material-ui/core/styles';
+import { Formik } from 'formik';
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
 import Dialog from '@material-ui/core/Dialog';
@@ -7,60 +9,130 @@ import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
+import axios from 'axios';
+import { Typography } from '@material-ui/core';
+
+const useStyles = makeStyles((theme) => ({
+  email: {
+    marginBottom: theme.spacing(1),
+  },
+  responseText: {
+    marginTop: theme.spacing(1),
+  }
+}));
+
+const POST_URL = 'https://getform.io/f/45545d77-d55a-40a5-8f48-d1352091992e';
 
 const FeedbackDialog = ({ open, cancel}) => {
-  const [val, setVal] = useState("");
-  const [err, setErr] = useState(false);
+  const classes = useStyles();
 
-  const handleClose = () => {
-  };
+  const [emailSent, setEmailSent] = useState(false);
+  const [emailSendError, setEmailSendError] = useState(false);
 
-  const handleTextChange = (feedback) => {
-    setVal(feedback);
-  };
-
-  const handleSubmit = () => {
-    if (val === '') {
-      setErr(true);
-    }
+  const sendEmail = (formData, setSubmitting, resetForm) => {
+    axios.post(POST_URL, formData)
+      .then(() => {
+        setSubmitting(false);
+        setEmailSent(true);
+        resetForm({ values: { email: '', message: '' }});
+        setTimeout(() => {
+          setEmailSent(false);
+          cancel();
+        }, 3000);
+      }, function() {
+        setEmailSendError(true);
+        setTimeout(() => {
+          setEmailSendError(false);
+        }, 3000);
+      });
   }
 
   return (
-    <div>
-      <Dialog open={open} onClose={handleClose} aria-labelledby="form-dialog-title">
-        <DialogTitle id="form-dialog-title">Feedback</DialogTitle>
-        <DialogContent>
-          <DialogContentText>
-            Feel free to share bugs, feature requests, ideas, or anything else you want to say!
-          </DialogContentText>
-          <TextField
-            id="outlined-multiline-static"
-            multiline
-            fullWidth
-            autoFocus
-            rows={6}
-            variant="filled"
-            value={val}
-            error={err}
-            onChange={(e) => handleTextChange(e.target.value)}
-          />
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={cancel} color="primary">
-            Cancel
-          </Button>
-          <Button onClick={handleSubmit} color="primary" variant="outlined">
-            Send
-          </Button>
-        </DialogActions>
-      </Dialog>
-    </div>
+    <Dialog open={open} aria-labelledby="form-dialog-title">
+      <DialogTitle id="form-dialog-title">Feedback</DialogTitle>
+      <DialogContent>
+        <DialogContentText>
+          Feel free to share bugs, feature requests, ideas, or anything else you want to say!
+        </DialogContentText>
+        <Formik
+          initialValues={{ email: '', message: '' }}
+          validate={values => {
+            const errors = {};
+            if (!values.message) {
+              errors.message = 'Required';
+            }
+            if (values.email.length) {
+              if (
+                !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+              ) {
+                errors.email = 'Invalid email address';
+              }
+            }
+            return errors;
+          }}
+          onSubmit={(values, { setSubmitting, resetForm }) => {
+              sendEmail(values, setSubmitting, resetForm);
+          }}
+        >
+          {({
+            values,
+            errors,
+            touched,
+            handleChange,
+            handleBlur,
+            handleSubmit,
+            isSubmitting,
+          }) => (
+            <form onSubmit={handleSubmit}>
+              <TextField
+                name="email"
+                placeholder="Email (optional)"
+                type="email"
+                className={classes.email}
+                autoFocus
+                fullWidth
+                autoFocus
+                variant="filled"
+                value={values.email}
+                error={errors.email}
+                onChange={handleChange}
+                onBlur={handleBlur}
+              />
+              {errors.email && touched.email && <Typography variant="body2" color="error">{errors.email}</Typography>}
+              <TextField
+                name="message"
+                placeholder="Message (required)"
+                multiline
+                fullWidth
+                rows={6}
+                variant="filled"
+                value={values.message}
+                error={errors.message}
+                onChange={handleChange}
+                onBlur={handleBlur}
+              />
+              {errors.message && touched.message && <Typography variant="body2" color="error">{errors.message}</Typography>}
+              {emailSent && <Typography variant="body1" color="primary" align="center" className={classes.responseText}>Email sent</Typography>}
+              {emailSendError && <Typography variant="body1" color="error" align="center" className={classes.responseText}>Something went wrong. Please try again.</Typography>}
+              <DialogActions>
+                <Button onClick={cancel} disabled={isSubmitting} color="primary">
+                  Cancel
+                </Button>
+                <Button onClick={handleSubmit} type="submit" disabled={isSubmitting} color="primary" variant="outlined">
+                  {isSubmitting ? 'Sending...' : 'Submit'}
+                </Button>
+              </DialogActions>
+            </form>
+          )}
+        </Formik>
+      </DialogContent>
+    </Dialog>
   );
 }
 
 FeedbackDialog.propTypes = {
-    open: PropTypes.func.isRequired,
-    cancel: PropTypes.func.isRequired,
+  open: PropTypes.func.isRequired,
+  cancel: PropTypes.func.isRequired,
 };
 
 export default FeedbackDialog;
